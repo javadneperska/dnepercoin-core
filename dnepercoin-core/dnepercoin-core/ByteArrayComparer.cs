d37fdf3f7b82bae209325d305338f86e7b992c57
javadneperska <javadneperska@gmail.com>
2019-12-18 12:15:01 +0100

    Add core logic

---
+++ dnepercoin-core/dnepercoin-core/ByteArrayComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnepercoin_core
+{
+    // https://stackoverflow.com/a/1440395
+    public class ByteArrayComparer : IEqualityComparer<byte[]>
+    {
+        public bool Equals(byte[] left, byte[] right)
+        {
+            if (left == null || right == null)
+            {
+                return left == right;
+            }
+            return left.SequenceEqual(right);
+        }
+        public int GetHashCode(byte[] key)
+        {
+            if (key == null)
+                throw new ArgumentNullException("key");
+            return key.Sum(b => b);
+        }
+    }
+}
--- dnepercoin-core/dnepercoin-core/Program.cs
+++ dnepercoin-core/dnepercoin-core/Program.cs
@@ -1,15 +1,302 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace dnepercoin_core
 {
     class Program
     {
+        public static uint GlobalDifficulty = 12;
+        public static byte[] LastBlockHash = null;
+        public static Dictionary<byte[], double> Balances = new Dictionary<byte[], double>(new ByteArrayComparer());
+        public static List<Block> Blocks = new List<Block>();
+        public static List<Transaction> Swarm = new List<Transaction>();
+        public static List<IPAddress> Clients = new List<IPAddress>();
         static void Main(string[] args)
         {
+            //Task.Run(() => Serve());
+
+            Discoverer.PeerJoined = x => Connect(x);
+            Discoverer.PeerLeft = x => Disconnect(x);
+            Discoverer.Start();
+
+            Task.Delay(5000).Wait();
+
+            //Task.Run(() => Update());
+
+            Task.Delay(5000).Wait();
+
+            if (!File.Exists("privatekey.txt"))
+            {
+                CngKeyCreationParameters keyCreationParameters = new CngKeyCreationParameters();
+                keyCreationParameters.ExportPolicy = CngExportPolicies.AllowPlaintextExport;
+                keyCreationParameters.KeyUsage = CngKeyUsages.Signing;
+
+                CngKey key = CngKey.Create(CngAlgorithm.ECDsaP256, null, keyCreationParameters);
+
+                ECDsaCng dsa = new ECDsaCng(key); //dsa = Digital Signature Algorithm
+                byte[] privateKey = dsa.Key.Export(CngKeyBlobFormat.EccPrivateBlob);
+                File.WriteAllText("privatekey.txt", String.Join(",", privateKey));
+            }
+
+            CngKey importedKey = CngKey.Import(File.ReadAllText("privatekey.txt").Split(',').Select(m => byte.Parse(m)).ToArray(), CngKeyBlobFormat.EccPrivateBlob);
+            ECDsaCng importedDSA = new ECDsaCng(importedKey);
+            byte[] publicKey = importedDSA.Key.Export(CngKeyBlobFormat.EccPublicBlob);
+            byte[] pubKeyHash = new byte[20];
+            bool isMining = false;
+            Thread miningThread = new Thread(() => Mine());
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                pubKeyHash = sha1.ComputeHash(publicKey);
+            }
+            Console.Write("Address: ");
+            for (int i = 0; i < 20; i++) 
+            {
+                Console.Write(pubKeyHash[i].ToString("X"));
+            }
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.Write("> ");
+                string cmd = Console.ReadLine();
+                string[] command = cmd.Split(' ');
+                switch (command[0])
+                {
+                    case "tx":
+                        {
+                            if (command.Length != 3 || command[1].Length != 40)
+                                break;
+                            byte[] addr = new byte[20];
+                            for (int i = 0; i < 20; i++)
+                            {
+                                addr[i] = Convert.ToByte(command[1].Substring(i * 2, 2), 16);
+                            }
+                            double amount = double.Parse(command[2]);
+
+                            var tx = new Transaction();
+                            tx.target = addr;
+                            tx.amount = amount;
+                            tx.source = publicKey;
+                            tx.timestamp = (uint)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                            tx.signature = importedDSA.SignData(tx.GetBytesToSign(), HashAlgorithmName.SHA256);
+
+                            foreach (IPAddress ip in Clients)
+                            {
+                                IPEndPoint ipep = new IPEndPoint(ip, 9050);
+                                UdpClient newsock = new UdpClient(ipep);
+                                byte[] data = null;
+
+                                byte[] transaction = tx.GetBytesTotal();
+
+                                List<byte> packet = new List<byte>();
+                                packet.Add(0x02);
+                                packet.AddRange(BitConverter.GetBytes((ushort)transaction.Length));
+                                packet.AddRange(transaction);
+
+                                newsock.Send(packet.ToArray(), packet.Count, ipep);
+
+                                data = newsock.Receive(ref ipep);
+                            }
+                            break;
+                        }
+                    case "mine":
+                        {
+                            if (isMining)
+                            {
+                                Console.WriteLine("Stopped mining.");
+                                miningThread.Abort();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Started mining.");
+                                miningThread.Start();
+                            }
+                            isMining = !isMining;
+                            break;
+                        }
+                }
+            }
+        }
+
+        private static void Disconnect(string x)
+        {
+            Clients.Remove(IPAddress.Parse(x));
+        }
+
+        static void Update()
+        {
+            Console.WriteLine("Running initial update...");
+            foreach (IPAddress ip in Clients)
+            {
+                IPEndPoint ipep = new IPEndPoint(ip, 9050);
+                UdpClient newsock = new UdpClient(ipep);
+                byte[] data = null;
+
+                newsock.Send(new byte[] { 0x03 }, 1, ipep);
+
+                data = newsock.Receive(ref ipep);
+
+                if (data != null)
+                {
+                    int num = BitConverter.ToInt32(data, 0);
+                    int index = 4;
+                    for (int i = 0; i < num; i++)
+                    {
+                        ushort len = BitConverter.ToUInt16(data, index);
+                        byte[] transaction = new byte[len];
+                        Array.Copy(data, index + 4, transaction, 0, len);
+                        var tx = Transaction.FromBytes(transaction, false);
+                        if (!Swarm.Any(x => x.signature.SequenceEqual(tx.signature)))
+                            Swarm.Add(tx);
+                        index += 4 + len;
+                    }
+                }
+            }
+            while (true)
+            {
+                foreach (IPAddress ip in Clients)
+                {
+                    IPEndPoint ipep = new IPEndPoint(ip, 9050);
+                    UdpClient newsock = new UdpClient(ipep);
+                    byte[] data = null;
+
+                    newsock.Send(new byte[] { 0x00 }, 1, ipep);
+
+                    data = newsock.Receive(ref ipep);
+
+                    if (data != null)
+                    {
+                        byte[] hash = new byte[32];
+                        Array.Copy(data, 0, hash, 0, 32);
+                        if (!hash.SequenceEqual(LastBlockHash) && !Blocks.Any(x => x.previousBlockHash.SequenceEqual(hash)))
+                        {
+                            while (!LastBlockHash.SequenceEqual(hash))
+                            {
+                                List<byte> packet = new List<byte>();
+                                packet.Add(0x01);
+                                packet.AddRange(LastBlockHash);
+                                newsock.Send(packet.ToArray(), packet.Count, ipep);
+
+                                data = newsock.Receive(ref ipep);
+
+                                int len = BitConverter.ToInt32(data, 0);
+                                if (len != 0)
+                                {
+                                    byte[] block = new byte[len];
+                                    Array.Copy(data, 4, block, 0, len);
+                                    var blk = Block.FromBytes(block);
+                                    if (blk != null)
+                                        Blocks.Add(blk);
+                                    else
+                                        break;
+                                }
+                                else
+                                    break;
+                            }
+                        }
+                    }
+                }
+
+                Task.Delay(5000).Wait();
+            }
+        }
+        static void Serve()
+        {
+            byte[] data = new byte[1024];
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            UdpClient newsock = new UdpClient(ipep);
+
+            while (true)
+            {
+                IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+                data = newsock.Receive(ref sender);
+
+                switch (data[0])
+                {
+                    case 0x00: // Request Last Block Hash
+                        {
+                            if (LastBlockHash != null)
+                                newsock.Send(LastBlockHash, 32, sender);
+                            else
+                            {
+                                byte[] NullBlock = new byte[32];
+                                newsock.Send(NullBlock, 32, sender);
+                            }
+                            break;
+                        }
+                    case 0x01: // Request Next Block
+                        {
+                            byte[] prevBlockHash = new byte[32];
+                            Array.Copy(data, 1, prevBlockHash, 0, 32);
+                            Block output = Blocks.First(x => x.previousBlockHash.SequenceEqual(prevBlockHash));
+                            if (output != null)
+                            {
+                                List<byte> packet = new List<byte>();
+                                packet.AddRange(BitConverter.GetBytes(output.originalData.Length));
+                                packet.AddRange(output.originalData);
+                                newsock.Send(packet.ToArray(), packet.Count, sender);
+                            }
+                            else
+                            {
+                                byte[] NullBlock = new byte[4];
+                                newsock.Send(NullBlock, 4, sender);
+                            }
+                            break;
+                        }
+                    case 0x02: // Add TX to swarm
+                        {
+                            ushort len = BitConverter.ToUInt16(data, 1);
+                            byte[] transaction = new byte[len];
+                            Array.Copy(data, 3, transaction, 0, len);
+                            var tx = Transaction.FromBytes(transaction, false);
+                            if (tx != null)
+                            {
+                                Swarm.Add(tx);
+                                newsock.Send(new byte[] { 0x01 }, 1, sender);
+                            }
+                            else
+                                newsock.Send(new byte[] { 0x00 }, 1, sender);
+                            break;
+                        }
+                    case 0x03: // Request swarm
+                        {
+                            List<byte> packet = new List<byte>();
+                            packet.AddRange(BitConverter.GetBytes(Swarm.Count));
+                            foreach (var tx in Swarm)
+                            {
+                                packet.AddRange(BitConverter.GetBytes((ushort)tx.originalData.Length));
+                                packet.AddRange(tx.originalData);
+                            }
+                            newsock.Send(packet.ToArray(), packet.Count, sender);
+                            break;
+                        }
+                }
+            }
+        }
+
+        static void Mine()
+        {
+            using (SHA256 sha = SHA256.Create())
+            {
+                while (true)
+                {
+                    
+                }
+            }
+        }
+
+        private static void Connect(IPAddress x)
+        {
+            Clients.Add(x);
         }
     }
 }
