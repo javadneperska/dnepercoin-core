82c5402b934b28dce4ffb711cfaa35da171fc1ee
javadneperska <javadneperska@gmail.com>
2019-12-18 08:17:55 +0100

    Create the project

---
+++ dnepercoin-core/dnepercoin-core/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnepercoin_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
